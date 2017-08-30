9203d78fcc02353c0f1f1910e9b16fc75543f9e0
dolrajgrg <grgraj99@gmail.com>
2017-08-30 11:53:14 +0545

    added index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Git example</title>
+</head>
+<body>
+	<div>
+		<ul>
+			<li>Samsung</li>
+			<li>HTC</li>
+			<li>Iphone</li>
+			<li>Nokia</li>
+			<li>Motorola</li>
+		</ul>
+	</div>
+</body>
+</html>
